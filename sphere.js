0319e82bbc61b3a3582868e57a7bda6d44187a52
tromili <anthonny.gonzales@gmail.com>
2018-08-03 18:14:28 -0500

    primer avance

--- sphere.js
+++ sphere.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', function() {
         scene.enablePhysics(gravityVector, physicsPlugin);
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    var camera = new BABYLON.FollowCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -35,14 +35,14 @@ window.addEventListener('DOMContentLoaded', function() {
     borderRight.position.y = 0.1
     borderRight.position.x = 4.9
     borderRight.position.z = 0
-    borderRight.physicsImpostor = new BABYLON.PhysicsImpostor(borderRight, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    borderRight.physicsImpostor = new BABYLON.PhysicsImpostor(borderRight, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 0.9 }, scene);
     
 
     borderLeft = BABYLON.MeshBuilder.CreateBox("box", {height: 0.1, width: 0.1, depth: 10, faceColors: [0,0,0,0]}, scene)
     borderLeft.position.y = 0.1
     borderLeft.position.x = -4.9
     borderLeft.position.z = 0
-    borderLeft.physicsImpostor = new BABYLON.PhysicsImpostor(borderLeft, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    borderLeft.physicsImpostor = new BABYLON.PhysicsImpostor(borderLeft, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 0.9 }, scene);
 
 
 
@@ -50,32 +50,32 @@ window.addEventListener('DOMContentLoaded', function() {
     borderTop.position.y = 0.1
     borderTop.position.x = 0
     borderTop.position.z = 4.9
-    borderTop.physicsImpostor = new BABYLON.PhysicsImpostor(borderTop, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    borderTop.physicsImpostor = new BABYLON.PhysicsImpostor(borderTop, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 0.9 }, scene);
 
     borderBottom = BABYLON.MeshBuilder.CreateBox("box", {height: 0.1, width: 9.5, depth: 0.1, faceColors: [0,0,0,0]}, scene)
     borderBottom.position.y = 0.1
     borderBottom.position.x = 0
     borderBottom.position.z = -4.9
-    borderBottom.physicsImpostor = new BABYLON.PhysicsImpostor(borderBottom, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    borderBottom.physicsImpostor = new BABYLON.PhysicsImpostor(borderBottom, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 0.9 }, scene);
 
 
 
 
-    var boxLeft = BABYLON.MeshBuilder.CreateBox("box", {height: 0.1, width: 0.1, depth: 1, faceColors: [0,0,0,0]}, scene);
+    var boxLeft = BABYLON.MeshBuilder.CreateBox("box", {height: 0.1, width: 0.1, depth: 3, faceColors: [0,0,0,0]}, scene);
     boxLeft.position.y = 0.1
     boxLeft.position.x = 3
     boxLeft.position.z = 1
-    boxLeft.physicsImpostor = new BABYLON.PhysicsImpostor(boxLeft, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 0.9 }, scene);
+    boxLeft.physicsImpostor = new BABYLON.PhysicsImpostor(boxLeft, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1000, restitution: 0.9 }, scene);
       //boxLeft.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(3, 0, 1));
-    boxLeft.physicsImpostor.applyImpulse(new BABYLON.Vector3(100, 0, 100), boxLeft.getAbsolutePosition());
+    boxLeft.physicsImpostor.applyImpulse(new BABYLON.Vector3(100, 100, 100), boxLeft.getAbsolutePosition());
 
-    var boxRight = BABYLON.MeshBuilder.CreateBox("box", {height: 0.1, width: 0.1, depth: 1, faceColors: [0,0,0,0]}, scene);
+    var boxRight = BABYLON.MeshBuilder.CreateBox("box", {height: 0.1, width: 0.1, depth: 3, faceColors: [0,0,0,0]}, scene);
     boxRight.position.y = 0.1
     boxRight.position.x = -3
     boxRight.position.z = 1
-    boxRight.physicsImpostor = new BABYLON.PhysicsImpostor(boxRight, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 0.9 }, scene)
+    boxRight.physicsImpostor = new BABYLON.PhysicsImpostor(boxRight, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1000, restitution: 0.9 }, scene)
       //boxRight.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(3, 0, 1))
-    boxRight.physicsImpostor.applyImpulse(new BABYLON.Vector3(100, 0, 100), boxRight.getAbsolutePosition());
+    boxRight.physicsImpostor.applyImpulse(new BABYLON.Vector3(100, 100, 100), boxRight.getAbsolutePosition());
 
     // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 0.2, scene);
@@ -84,7 +84,8 @@ window.addEventListener('DOMContentLoaded', function() {
     sphere.position.z = 0;
 
     sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 8, restitution: 0.9 }, scene);
-    sphere.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(3, 0, 1));
+    sphere.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(4, 0, 0));
+    sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(10, 10, 0), sphere.getAbsolutePosition());
     // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
     //var ground = BABYLON.Mesh.CreateGround("ground1",{height:10, width:10, subdivisions: 1, faceColors: [230,230,230,1]}, scene);
     var ground = BABYLON.MeshBuilder.CreateGround('ground1', {height:10, width:10, subdivisions: 1, faceColors: [230,230,230,1]}, scene)
